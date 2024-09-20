f8d0af3d6b1b30133e576930a115a8f584a26bb3
mgd <mgd@posteo.net>
2024-09-20 21:53:32 +0100

    Added endpoint to delete users by Id

--- src/main/java/com/mg/apidemo/controllers/UserController.java
+++ src/main/java/com/mg/apidemo/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.mg.apidemo.exceptions.UserNotFoundException;
 import com.mg.apidemo.entities.UserEntity;
 import com.mg.apidemo.repositories.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -47,6 +48,12 @@ public class UserController {
     @GetMapping("/user/{id}")
     public UserEntity getUserById(@PathVariable Integer id) throws UserNotFoundException {
         return userEntityRepository.getUserEntityById(id).orElseThrow(() -> new UserNotFoundException("No such entity"));
+    }
+
+    @DeleteMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Integer id) {
+        userEntityRepository.deleteById(id);
     }
 
 }
